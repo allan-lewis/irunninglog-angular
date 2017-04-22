b7720bd9889e406d35fd306fa416d2003d4ba649
Allan Lewis <allan@irunninglog.com>
2017-04-21 20:41:16 -0400

    #3 - @ngrx

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as title 'irunninglog'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.toolbarTitle).toEqual('irunninglog');
   }));
 
   it('should render title in a h1 tag', async(() => {
