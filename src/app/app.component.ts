1945ade41b5e63ff88d762337ef73558073ed766
Allan Lewis <allan@irunninglog.com>
2017-04-21 20:30:16 -0400

    #3 - @ngrx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,21 @@
 import { Component } from '@angular/core';
-
 import {Auth} from './auth.service';
-import { Store } from '@ngrx/store';
+import {Store} from '@ngrx/store';
 import {Observable} from "rxjs";
 import {AppState} from './app.state';
 
 @Component({
-  selector: 'app-root',
+  selector: 'irl-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   toolbarTitle = 'irunninglog';
   toolbarSubTitle = 'cuatro';
-  title = 'app works!';
   username: Observable<string>;
 
   constructor(public auth: Auth, private store: Store<AppState>) {
-    this.username = store.select('username');
+    this.username = store.select(state => state.username);
   }
 
 }
-
