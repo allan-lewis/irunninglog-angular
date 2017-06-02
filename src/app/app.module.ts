c5bd5daa4a93f298720ccd1155cd5d4a0a581148
irunninglog <allan@irunninglog.com>
2017-06-01 22:22:52 -0400

    #21 - Project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
+// ~~ ANGULAR ======================
+
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, Http, RequestOptions } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+
+// ~~ MATERIAL =====================
+
+import 'hammerjs';
+import { MdToolbarModule, MdCardModule, MdButtonModule, MdIconModule, MdDialogModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// ~~ COMPONENTS ===================
+
+import { ChartComponent } from './chart/chart.component';
+import { GoalsComponent } from './goals/goals.component';
+import { LoginComponent } from './login/login.component';
+import { PingComponent } from './ping/ping.component';
+import { ShoesComponent } from './shoes/shoes.component';
+import { StreaksComponent } from './streaks/streaks.component';
+
+// ~~ UNORGANIZED ==================
 
 import { HeaderComponent } from './header.component';
-import { LoginComponent } from './login.component';
 import { PageComponent } from './page.component';
 import { MainComponent } from './main.component';
 import { ProfileComponent } from './profile.component';
 import { LogoutComponent } from './logout.component';
-import { PingComponent } from './ping.component';
-import { StreaksComponent } from './streaks.component';
-import { ChartComponent } from './chart.component';
-import { ShoesComponent } from './shoes.component';
-import { GoalsComponent } from './goals.component';
 import { ConfirmDialog } from './dialog.component';  
-
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
-import { MdToolbarModule, MdCardModule, MdButtonModule, MdIconModule, MdDialogModule } from '@angular/material';
-
-import 'hammerjs';
 
 import { LoginService } from "./login.service";
 import { ProfileService } from "./profile.service";
