4e33c9a42db81678c78d79361a53b2db43709213
irunninglog <allan@irunninglog.com>
2017-07-12 22:13:05 -0400

    #28 - Timezone parameter is included in outgoing requests

--- src/app/http/request-options.service.ts
+++ src/app/http/request-options.service.ts
@@ -11,6 +11,7 @@ export class AppRequestOptions extends BaseRequestOptions {
     super();
 
     this.headers.set('Content-Type', 'application/json');
+    this.headers.set('IRL-TimeZone-Offset', new Date().getTimezoneOffset().toString());
 
     store.select(state => state.authentication).filter(x => !!x.token).subscribe(x => this.headers.set('Authorization', 'Bearer ' + x.token));
   }
