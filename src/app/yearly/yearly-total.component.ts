b54e91ac09d4420a302a61335566b6c51492b2eb
irunninglog <allan@irunninglog.com>
2017-11-27 23:03:28 -0500

    #65 - Yearly progress components have been redesigned

--- src/app/yearly/yearly-total.component.ts
+++ src/app/yearly/yearly-total.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnChanges, Input, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnChanges, Input, ViewChild, ElementRef, ViewEncapsulation } from '@angular/core';
 import { YearlyTotalModel } from '../state/yearly-total.model';
 import * as d3 from 'd3';
 
 @Component({
   selector: 'irl-component-yearly-total',
   templateUrl: './yearly-total.component.html',
-  styleUrls: ['./yearly-total.component.css']
+  styleUrls: ['./yearly-total.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class YearlyTotalComponent implements OnChanges {
 
-  @Input() model : YearlyTotalModel;
+  @Input() model: YearlyTotalModel;
   @ViewChild('chart') private chartContainer: ElementRef;
 
   ngOnChanges() {
@@ -18,16 +19,41 @@ export class YearlyTotalComponent implements OnChanges {
   }
 
   drawChart() {
-      const element = this.chartContainer.nativeElement;
+    const element = this.chartContainer.nativeElement;
 
-      const svg = d3.select(element).append('svg')
-        .attr('width', element.offsetWidth)
-        .attr('height', element.offsetHeight);
+    var w = element.offsetWidth, h = element.offsetHeight;
 
-      const circle = svg.append('circle')
-        .attr('cx', 50).attr('cy', 50)
-        .attr('r', this.model.percentage / 2)
-        .attr('style', 'fill: rgba(67,160,71, ' + (this.model.percentage / 100) + ')');
+    const svg = d3.select(element).append('svg')
+      .attr('width', w)
+      .attr('height', h)
+      .attr('class', 'shadow')
+      .append('g')
+      .attr('transform', 'translate(' + w / 2 + ',' + h / 2 + ')');
+
+    var outerRadius = (w / 2) - 10;
+    var innerRadius = outerRadius - 8;
+
+    var color = ['#ec1561', '#2a3a46', '#202b33'];
+
+    var arc = d3.arc()
+      .innerRadius(innerRadius)
+      .outerRadius(outerRadius)
+      .startAngle(0)
+      .endAngle(2 * Math.PI);
+
+    var arcLine = d3.arc()
+      .innerRadius(innerRadius)
+      .outerRadius(outerRadius)
+      .startAngle(0);
+
+    var path = svg.append('path')
+      .attr('d', arc)
+      .attr('style', 'fill: #eee');
+
+    var pathForeground = svg.append('path')
+      .datum({ endAngle: (this.model.percentage / 100) * (2 * Math.PI) })
+      .attr('d', arcLine)
+      .attr('style', 'fill: #ff5722');
   }
 
 }
